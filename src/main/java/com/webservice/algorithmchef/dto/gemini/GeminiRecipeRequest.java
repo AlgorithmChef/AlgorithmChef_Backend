6404a1bad6f16c270948d88d9d395ff5281c41ac
kghc4232 <kghc4232@kyonggi.ac.kr>
2025-11-20 15:11:37 +0900

    25.11.20
    fork 레포지토리에 분기 후 첫 commit

--- src/main/java/com/webservice/algorithmchef/dto/gemini/GeminiRecipeRequest.java
+++ src/main/java/com/webservice/algorithmchef/dto/gemini/GeminiRecipeRequest.java
@@ -8,19 +8,21 @@ public class GeminiRecipeRequest {
     public record PreferRequest(
             Long userPkId, // DB의 User PK (id)
             List<String> excludedTitles
+            // client의 첫 요청시 gemini는 비어있는 string 리스트 반환.
+            // 재요청 시 front에서 요리명만 해당 리스트에 담아 전송. 그러면 gemini는 해당 레시피는 제외하여 탐색.
     ) {}
 
     // 2. Speech-to-Text 조건 기반 추천 요청
     public record ConditionRequest(
             Long userPkId,
             List<String> excludedTitles,
-            String condition // 기분, 상황
+            String condition // 기분, 상황 등 조건
     ) {}
 
     // 3. 임박한 재료 추천 요청
     public record ExpirRequest(
             Long userPkId,
             List<String> excludedTitles,
-            List<String> ingredients // 냉장고 재료
+            List<String> ingredients // 소비기한 임박 재료
     ) {}
 }
