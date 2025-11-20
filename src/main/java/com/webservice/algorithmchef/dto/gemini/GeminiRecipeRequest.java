6b60467e95d9c18a15b3b2039976fb69ed997039
kghc4232 <kghc4232@kyonggi.ac.kr>
2025-11-20 15:32:03 +0900

    25.11.20
    fork 레포지토리에 분기 후 첫 commit

--- src/main/java/com/webservice/algorithmchef/dto/gemini/GeminiRecipeRequest.java
+++ src/main/java/com/webservice/algorithmchef/dto/gemini/GeminiRecipeRequest.java
@@ -8,7 +8,7 @@ public class GeminiRecipeRequest {
     public record PreferRequest(
             Long userPkId, // DB의 User PK (id)
             List<String> excludedTitles
-            // client의 첫 요청시 gemini는 비어있는 string 리스트 반환.
+            // client의 첫 요청시 string 리스트는 비어있음.
             // 재요청 시 front에서 요리명만 해당 리스트에 담아 전송. 그러면 gemini는 해당 레시피는 제외하여 탐색.
     ) {}
 
